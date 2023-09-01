36aaf655c1c3fb642634ab517d97fe76eaf16fd1
Andrej <antandrej2003@gmail.com>
2023-09-01 02:01:18 +0200

    code cleanup

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -31,36 +31,36 @@ const get10Reviews = async (req, res, next) => {
 };
 
 const getReviewsByType = async (req, res, next) => {
-        try {
-            const params = [req.params.by];
-            const type = req.params.type;
+    try {
+        const params = [req.params.by];
+        const type = req.params.type;
 
-            const query = `SELECT * FROM reviews WHERE ${type} = $1 ORDER BY date DESC`;
-    
-            const sessions = await client.query(query, params);
-    
-            res.send(sessions.rows);
-    
-            client.end;
-        }
-        catch (err) {
-            console.log(err.message);
-        }
+        const query = `SELECT * FROM reviews WHERE ${type} = $1 ORDER BY date DESC`;
+
+        const sessions = await client.query(query, params);
+
+        res.send(sessions.rows);
+
+        client.end;
+    }
+    catch (err) {
+        console.log(err.message);
+    }
 };
 
 
 const newReview = async (req, res, next) => {
-    try{
+    try {
         const params = [req.body.name, req.body.bus, req.body.stars, req.body.comment, req.body.date];
         const query = `INSERT INTO reviews(name, bus, stars, comment, date) VALUES($1, $2, $3, $4, $5)`;
 
-        const review = await client.query(query,params);
+        const review = await client.query(query, params);
 
         res.send("Review Added! \n");
-        
+
         client.end;
     }
-    catch(err){
+    catch (err) {
         console.log(err.message);
     }
 };
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router  = express.Router(); 
+const router = express.Router();
 const reviewsController = require('../controllers/reviews');
 
 router.get('/api/reviews', reviewsController.getAllReviews);
