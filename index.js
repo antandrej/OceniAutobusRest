b9972aed0a5340fd8c4dfef09d7dea24de1ca926
Andrej <antandrej2003@gmail.com>
2023-09-21 21:38:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const client = require('./connection.js')
 const app = require('./app.js');
 
 const reviewsRouter = require('./routes/reviews.js');
-//const commentsRouter = require('./routes/comments.js');
+const commentsRouter = require('./routes/comments.js');
 
 app.use('/', reviewsRouter);
-//app.use('/', commentsRouter);
+app.use('/', commentsRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} . . .`));
