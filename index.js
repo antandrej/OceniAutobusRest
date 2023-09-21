5f8870a3825b2c4b4d6535bedec4e16d2860870b
Andrej <antandrej2003@gmail.com>
2023-09-21 20:13:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,8 @@ const reviewsRouter = require('./routes/reviews.js');
 
 app.use('/', reviewsRouter);
 //app.use('/', commentsRouter);
-/*
-(async () => {
-    await client.connect();
-    try {
-      const results = await client.query("SELECT NOW()");
-      console.log(results);
-    } catch (err) {
-      console.error("error executing query:", err);
-    } finally {
-      client.end();
-    }
-  })();
-*/
-const port = process.env.PORT || 26257;
-app.listen(26257, () => console.log(`Listening on port ${port} . . .`));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} . . .`));
 
 client.connect();
